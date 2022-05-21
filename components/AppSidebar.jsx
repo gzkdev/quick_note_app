0dc086837c79c8c26b27eff8d50172eee22bf9ab
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-21 23:32:05 +0100

    added appsidebar component, styles

--- components/AppSidebar.jsx
+++ components/AppSidebar.jsx
@@ -1,38 +1,45 @@
 import Link from "next/link";
+import { useState } from "react";
+import styles from "../styles/appSidebar.module.css";
 
 export default function AppSidebar() {
+  const [isOpen, setIsOpen] = useState(true);
+  const toggleIsOpen = () => setIsOpen((prev) => !prev);
+
   return (
-    <div>
-      <span>Notebook</span>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>All Notes</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a>Favorites</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a>Trash</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a>All Notes</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a>Categories</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className={styles.sidebar}>
+      <div className={styles.sidebar__container}>
+        <span>Notebook</span>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">
+                <a>All Notes</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Favorites</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Trash</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>All Notes</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Categories</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import styles from '../styles/Home.module.css'
 import AppHead from '../components/AppHead'
 import NotesContextProvider from '../Data/NotesContextProvider'
+import AppSidebar from '../components/AppSidebar'
 
 export default function Home() {
   return (
     <NotesContextProvider>
       <AppHead />
-      <div className={styles.container}>
+      <div className="app">
+        <AppSidebar />
         <h1>Hello world</h1>
       </div>
     </NotesContextProvider>
