9bbd256f07b67deceedbc007899cd0764148075d
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-22 00:13:39 +0100

    added notes container styles, updated global container styles

--- components/AppNotesContainer.jsx
+++ components/AppNotesContainer.jsx
@@ -1,4 +1,5 @@
 import styles from "../styles/appNotesContainer.module.css";
+import AppSearchbar from "./AppSearchbar";
 
 export default function AppNotesContainer() {
   return (
@@ -7,6 +8,7 @@ export default function AppNotesContainer() {
         <span>Hello dear 👋🏾</span>
         <h1>All Notes</h1>
       </div>
+      <AppSearchbar />
       <div className={styles.notes__container}>
         <div className={styles.app__note}></div>
       </div>
