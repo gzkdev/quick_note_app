f855dbf3bbbf2a1ac42362c494f1aa2a47c982e6
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-22 18:30:04 +0100

    updated appHead component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import AppHead from "../components/AppHead"
 // import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      {/* <AppHead /> */}
+      <AppHead />
       <h1>Hello world</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptates odio eligendi quae magnam, ratione maxime est neque illum dolorum a ipsa dolore voluptate, sit nostrum pariatur possimus numquam molestiae nesciunt ipsum unde labore. Voluptatum qui culpa, rerum vero ex ipsa magni omnis nemo corrupti iste ratione illo laborum asperiores.</p>
     </div>
