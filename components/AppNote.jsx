3d1496cbd1b0b5c3578ac1942699743558378fcc
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-23 00:53:48 +0100

    updated appnote structure and styles

--- components/AppNote.jsx
+++ components/AppNote.jsx
@@ -1,13 +1,15 @@
 import styles from "../styles/appNote.module.css";
+import { DotsVerticalIcon, Pencil1Icon } from "@radix-ui/react-icons";
 
 export default function AppNote({ data }) {
   return (
     <div className={styles.note}>
       <div className={styles.note__header}>
         <span className={styles.note__title}>{data.title}</span>
-        <button className={styles.note__edit_btn}>Edit</button>
         <div className={styles.note__actions}>
-          <button></button>
+          <button>
+            <DotsVerticalIcon />
+          </button>
           <div className={styles.note__actions_menu}>
             <button>Add to Favorites</button>
             <button>Delete</button>
@@ -17,6 +19,9 @@ export default function AppNote({ data }) {
       <p className={styles.note__content}>{data.content}</p>
       <div className={styles.note__footer}>
         <small>{data.date}</small>
+        <button className={styles.note__edit_btn}>
+          <Pencil1Icon />
+        </button>
       </div>
     </div>
   );
