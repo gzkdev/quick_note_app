9a17d97d2a64ac850a236710e5b5f6b8b7c664a7
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-23 00:15:55 +0100

    updated add note component, and styles

--- components/AddNoteForm.jsx
+++ components/AddNoteForm.jsx
@@ -2,10 +2,9 @@ import { useNotesContext } from "../context/NotesContextProvider";
 import styles from "../styles/addNoteForm.module.css";
 import { useRef } from "react";
 
-export default function AddNoteForm() {
+const AddNoteForm = () => {
   const { isAddNoteFormOpen, toggleIsAddNoteFormOpen, addNote } =
     useNotesContext();
-
   const noteTitle = useRef();
   const noteText = useRef();
 
@@ -21,37 +20,40 @@ export default function AddNoteForm() {
       };
       addNote(data);
       noteTitle.current.value = noteText.current.value = "";
+      toggleIsAddNoteFormOpen();
     }
-
-    return (
-      <div data-toggle={isAddNoteFormOpen} className={styles.overlay}>
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <div className={styles.form__header}>
-            <button
-              className={styles.form__button}
-              onClick={toggleIsAddNoteFormOpen}
-            >
-              Close
-            </button>
-            <button type="submit" className={styles.form__button}>
-              Save
-            </button>
-          </div>
-          <input
-            ref={noteTitle}
-            className={styles.form__input}
-            type="text"
-            name="title"
-            placeholder="Note title"
-          />
-          <textarea
-            ref={noteText}
-            className={styles.form__textarea}
-            name="content"
-            placeholder="Write something"
-          ></textarea>
-        </form>
-      </div>
-    );
   };
-}
+
+  return (
+    <div data-toggle={isAddNoteFormOpen} className={styles.overlay}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.form__header}>
+          <button
+            className={styles.form__button}
+            onClick={toggleIsAddNoteFormOpen}
+          >
+            Close
+          </button>
+          <button type="submit" className={styles.form__button}>
+            Save
+          </button>
+        </div>
+        <input
+          ref={noteTitle}
+          className={styles.form__input}
+          type="text"
+          name="title"
+          placeholder="Note title"
+        />
+        <textarea
+          ref={noteText}
+          className={styles.form__textarea}
+          name="content"
+          placeholder="Write something"
+        ></textarea>
+      </form>
+    </div>
+  );
+};
+
+export default AddNoteForm;
