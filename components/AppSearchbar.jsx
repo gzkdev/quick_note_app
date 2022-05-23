421e19d1b2ec37af6ed50f89d75f504080e77f05
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-05-23 01:04:47 +0100

    added search functionality

--- components/AppSearchbar.jsx
+++ components/AppSearchbar.jsx
@@ -1,11 +1,15 @@
-import { useNotesContext } from "../context/NotesContextProvider";
 import styles from "../styles/appSearchbar.module.css";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
 export default function AppSearchbar({ state, stateFunction, toggle }) {
+  const handleOnChange = (e) => {
+    stateFunction(e.target.value);
+  };
+
   return (
     <div className={styles.searchbar}>
       <input
+        onChange={handleOnChange}
         className={styles.searchbar__input}
         type="text"
         name="search"
